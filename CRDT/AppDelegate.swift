038c855ae7cf7b11e5ce85f586aa8b524f72b9d9
James Tang <jamestang@invisionapp.com>
2018-04-15 23:22:16 +0800

    Initial Commit

---
+++ CRDT/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CRDT
+//
+//  Created by invision on 15/4/2018.
+//  Copyright © 2018 Invision. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CRDT/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CRDT
+//
+//  Created by invision on 15/4/2018.
+//  Copyright © 2018 Invision. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
